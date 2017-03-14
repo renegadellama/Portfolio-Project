621f4f6de58b533b7924e3f7714c051568178e96
Isak Swearingen <isakswearingen@gmail.com>
2017-03-14 13:33:41 -0700

    getting started

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function portfolioData(projName, icon, description, date, contributors, link){
+  this.projName = projName;
+  this.icon = icon;
+  this.description = description;
+  this.date = date;
+  this.contributors = contributors;
+  this.link = link;
+}
+
+var histicuffs = ('Histicuffs', 'img', 'Historical Quiz-Based Figthing Tournament', '3/3/17', 'Isak Swearingen, Erik Enderlein, James Thomas, Enrique Rico', 'https://endere.github.io/Histicuffs/');
