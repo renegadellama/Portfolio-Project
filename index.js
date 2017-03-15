e8a738ce25675d3542e59313ba8e594dfc33076c
Isak Swearingen <isakswearingen@gmail.com>
2017-03-15 10:47:40 -0700

    got css working with desktop and mobile views, have constructor funtion

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 'use strict';
+var portfolio = [];
 
 function portfolioData(projName, icon, description, date, contributors, link){
   this.projName = projName;
@@ -10,3 +11,4 @@ function portfolioData(projName, icon, description, date, contributors, link){
 }
 
 var histicuffs = ('Histicuffs', 'img', 'Historical Quiz-Based Figthing Tournament', '3/3/17', 'Isak Swearingen, Erik Enderlein, James Thomas, Enrique Rico', 'https://endere.github.io/Histicuffs/');
+var salmoncookies = ('Salmon Cookies', 'img', 'Random Number Generator which Populates Data for an Imaginary Store', '2/17/16', 'Isak Swearingen', 'https://github.com/renegadellama/Salmon-Cookies' );
