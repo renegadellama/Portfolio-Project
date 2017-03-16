e01595353ed9dfc96d0fee712b96ed4986d6bcaa
Isak Swearingen <isakswearingen@gmail.com>
2017-03-16 12:42:27 -0700

    added sourcedata, index, started work on constructor function

---
+++ sourceData.js
@@ -0,0 +1,5 @@
+'use strict';
+
+var histicuffs = new PortfolioData('Histicuffs', 'Images/teddy.jpg', 'Historical Quiz-Based Figthing Tournament', '3/3/17', 'Isak Swearingen, Erik Enderlein, James Thomas, Enrique Rico', 'https://endere.github.io/Histicuffs/');
+var salmoncookies = new PortfolioData('Salmon Cookies', 'Images/salmon.png', 'Random Number Generator which Populates Data for an Imaginary Store', '2/17/16', 'Isak Swearingen', 'https://github.com/renegadellama/Salmon-Cookies' );
+var busmall = new PortfolioData('BusMall', 'img', 'Selects from a variety of images, then returns click results and populates to a graph.', '2/24/17', 'Isak Swearingen', 'https://github.com/renegadellama/busmall');
