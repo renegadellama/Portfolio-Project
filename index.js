86296571f286d3e5f2cb71220adf2a7596b3ba2b
Isak Swearingen <isakswearingen@gmail.com>
2017-03-16 10:17:33 -0700

    initial commit day 2

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 'use strict';
 var portfolio = [];
 
-function portfolioData(projName, icon, description, date, contributors, link){
-  this.projName = projName;
-  this.icon = icon;
-  this.description = description;
-  this.date = date;
-  this.contributors = contributors;
-  this.link = link;
+function portfolioData(obj){
+  this.projName = obj.projName;
+  this.icon = obj.icon;
+  this.description = obj.description;
+  this.date = obj.date;
+  this.contributors = obj.contributors;
+  this.link = obj.link;
 }
 
-var histicuffs = ('Histicuffs', 'img', 'Historical Quiz-Based Figthing Tournament', '3/3/17', 'Isak Swearingen, Erik Enderlein, James Thomas, Enrique Rico', 'https://endere.github.io/Histicuffs/');
+var histicuffs = ('Histicuffs', Images/teddy.jpg, 'Historical Quiz-Based Figthing Tournament', '3/3/17', 'Isak Swearingen, Erik Enderlein, James Thomas, Enrique Rico', 'https://endere.github.io/Histicuffs/');
 var salmoncookies = ('Salmon Cookies', 'img', 'Random Number Generator which Populates Data for an Imaginary Store', '2/17/16', 'Isak Swearingen', 'https://github.com/renegadellama/Salmon-Cookies' );
