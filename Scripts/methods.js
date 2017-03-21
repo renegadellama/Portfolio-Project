e88a9b9fe6abd942d1b115210920864406a33e19
Isak Swearingen <isakswearingen@gmail.com>
2017-03-21 11:32:11 -0700

    fixed css layout

--- Scripts/methods.js
+++ Scripts/methods.js
@@ -1,4 +1,5 @@
 'use strict';
+var myProjects = [];
 
 PortfolioData.prototype.createImage = function(i) {
   var imageEl = document.createElement('img');
@@ -6,10 +7,48 @@ PortfolioData.prototype.createImage = function(i) {
 };
 
 PortfolioData.prototype.toHtml = function() {
-  var $newProject = $('article.about-me').clone().removeClass('about-me');
-  $newProject.find('h3').text(this.projName);
-  $newProject.find('.project-image').attr('src', this.icon);
-  $newProject.find('.description').text(this.description);
-  $newProject.find('.date').text(this.date);
-  return $newProject;
+  var source = $('#project-template').text();
+  var templateRender = Handlebars.compile(source);
+
+  return (templateRender(this));
 };
+
+projects.forEach(function(projectObject){
+  myProjects.push(new PortfolioData(projectObject));
+});
+
+function showHome(){
+  $('#homeclick').on('click', function(){
+    $('.about-me').fadeOut();
+    $('.my-projects').fadeOut();
+  });
+}
+
+function showAbout(){
+  $('#aboutclick').on('click', function(){
+    $('.about-me').delay(500).fadeIn();
+    $('.my-projects').fadeOut();
+  });
+};
+
+function showPortfolio(){
+  $('#projectclick').on('click', function(){
+    $('.my-projects').delay(500).fadeIn();
+    $('.about-me').fadeOut();
+  });
+}
+
+function hideOnLoad(){
+  $('.my-projects').hide();
+  $('#project-template').hide();
+  $('.about-me').hide();
+}
+
+hideOnLoad();
+showHome();
+showAbout();
+showPortfolio();
+
+myProjects.forEach(function(a) {
+  $('.my-articles').append(a.toHtml());
+});
